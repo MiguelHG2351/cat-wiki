f032d482f2a06de05019abb048c493751e2f7b44
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-12-20 19:39:28 +0000

    evento resize por si se activa e menu mobile y luego la pantalla pasar a ser escritorio

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+import { useState } from 'react'
+import { QueryClientProvider, Hydrate, QueryClient } from 'react-query'
 import 'apps/style.css'
 import Layout from 'components/container/Layout'
 
 export default function MyApp({ Component, pageProps }) {
+  const [ queryClient ] = useState(() => new QueryClient())
+  
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
+import { dehydrate, QueryClient, useQuery } from "react-query"
 import Head from "next/head"
 import Image from "next/image"
 import Form from "components/Home/Form"
 
-export async function getStaticProps() {
-	console.log(process.env.CAT_API_TOKEN)
-	const breeds = await fetch('https://api.thecatapi.com/v1/images/search?format=json', {
-		headers: {
-			'x-api-key': process.env.CAT_API_TOKEN || ''
-		}
-	})
-	const data = await breeds.json()
+// export async function getStaticProps() {
+//   const queryClient = new QueryClient()
 
-	return {
-		props: {
-			name: data,
-      token: process.env.CAT_API_TOKEN
-		}
-	}
-}
+//   // await queryClient.prefetchQuery('breeds', async () => {
+// 	console.log(process.env.CAT_API_TOKEN)
+// 	const breeds = await fetch('https://api.thecatapi.com/v1/images/search?format=json', {
+// 		headers: {
+// 			'x-api-key': process.env.CAT_API_TOKEN || ''
+// 		}
+// 	})
+// 	const data = await breeds.json()
+
+// 	return {
+// 		props: {
+// 			name: data,
+//       token: process.env.CAT_API_TOKEN
+// 		}
+// 	}
+// }
 
 export default function Home({ name, token }) {
   console.log(token)
@@ -28,7 +32,7 @@ export default function Home({ name, token }) {
       </Head>
      <section className="hero py-2 px-7 rounded-t-3xl bg-[url('/images/HeroImagesm.png')] bg-no-repeat bg-cover">
       <div className="left w-1/2">
-        <Image className="w-auto" src="/icons/CatwikiLogo2.svg" width={64} height={34.9} alt="Otra imagen del gato" />
+        <Image className="h-[35px] w-16" src="/icons/CatwikiLogo2.svg" width={64} height={35} alt="Otra imagen del gato" />
         <p className="text-white text-xs mb-2">Get to know more about your cat breed</p>
         <Form />
       </div>
