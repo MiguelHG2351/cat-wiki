9a0b9863507425d958e09c35ba26ca25122f1617
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-12-21 21:15:52 +0000

    remplazando ch a calc para calcular la ubicacion del icono

--- components/Home/Form.js
+++ components/Home/Form.js
@@ -44,11 +44,13 @@ export default function Form({ data }) {
 
 	return (
 		<>
-			<form className='relative'>
-				<div className="icon absolute left-[9ch] top-1/2 -translate-y-1/2">
-					<svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox='0 0 48 48'><path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z" /></svg>
+			<form>
+				<div className="relative inline-block">
+					<div className="icon absolute left-[calc(100%_-_28px)] top-1/2 -translate-y-1/2">
+						<svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox='0 0 48 48'><path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z" /></svg>
+					</div>
+					<input ref={ desktopInputRef } onClick={ toggleMenuMobile } type="text" className="inline-block w-[16ch] rounded-3xl py-2 px-3 outline-none placeholder:text-black" placeholder="Search" />
 				</div>
-				<input ref={ desktopInputRef } onClick={ toggleMenuMobile } type="text" className="inline-block w-[12ch] rounded-3xl py-2 px-3 outline-none placeholder:text-black" placeholder="Search" />
 				<div style={ { display: menu ? 'block' : 'none' } } className="modal-mobile fixed top-0 left-0 right-0 bottom-0 py-2 px-4 bg-white">
 					<div className="close-menu flex justify-end">
 						<button onClick={ toggleMenuMobile } type="button" className='bg-tertiary rounded-lg p-2'>
