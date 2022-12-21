ae8bc56f681f472d822d93468facd04c2f5f73cc
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-12-21 21:02:34 +0000

    formulario mmobile funcionando con los datos de react query

--- components/Home/Form.js
+++ components/Home/Form.js
@@ -1,14 +1,17 @@
+import Link from 'next/link'
 import { useState, useRef, useEffect } from 'react'
 import React from 'react'
 
-export default function Form({ name }) {
+//#region Styles
+
+export default function Form({ data }) {
 	const mobileInputRef = useRef(null)
 	const desktopInputRef = useRef(null)
-	console.log(name)
 	const [menu, setMenu] = useState(false)
+	const [search, setSearch] = useState(data.slice(0, 5))
 
 	function toggleMenuMobile() {
-		if(window.innerWidth < 768) {
+		if (window.innerWidth < 768) {
 			if (!menu) {
 				console.log('blur')
 				desktopInputRef.current.blur()
@@ -19,9 +22,19 @@ export default function Form({ name }) {
 			setMenu(!menu)
 		}
 	}
+
+	function onInputHandler(e) {
+		const value = e.target.value.toLowerCase()
+		const filtered = data.filter((item) => {
+			return item.name.toLowerCase().includes(value)
+		})
+		setSearch(filtered.slice(0, 5))
+		console.log()
+	}
+
 	useEffect(() => {
 		function handleResize() {
-			if(window.innerWidth > 768) {
+			if (window.innerWidth > 768) {
 				setMenu(false)
 			}
 		}
@@ -32,20 +45,31 @@ export default function Form({ name }) {
 	return (
 		<>
 			<form>
-				<input ref={desktopInputRef} onClick={ toggleMenuMobile } type="text" className="inline-block w-[12ch] rounded-3xl py-2 px-3 outline-none placeholder:text-black" placeholder="Search" />
+				<input ref={ desktopInputRef } onClick={ toggleMenuMobile } type="text" className="inline-block w-[12ch] rounded-3xl py-2 px-3 outline-none placeholder:text-black" placeholder="Search" />
+				<div style={ { display: menu ? 'block' : 'none' } } className="modal-mobile fixed top-0 left-0 right-0 bottom-0 py-2 px-4 bg-white">
+					<div className="close-menu flex justify-end">
+						<button onClick={ toggleMenuMobile } type="button" className='bg-tertiary rounded-lg p-2'>
+							<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox='0 0 48 48'><path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" /></svg>
+						</button>
+					</div>
+					<div className="input-mobile-container group relative">
+						<input onInput={ onInputHandler } ref={ mobileInputRef } className=" border-black border w-full my-7 rounded-lg py-2 px-3 placeholder:text-black" type="text" name="search-modal" placeholder="Search for a cat" />
+						<div className="results absolute top-[calc(100%_-_28px)] py-4 left-0 right-0 hidden group-focus-within:block hover:block">
+							<ul>
+								{ search.map(cat => (
+									<li key={cat.reference_image_id + cat.country_code}>
+										<Link className='block p-2' href="/">
+											{ cat.name }
+										</Link>
+									</li>
+								)) }
+							</ul>
+						</div>
+					</div>
+				</div>
 			</form>
-			<div style={{display: menu ? 'block' : 'none'}} className="modal-mobile fixed top-0 left-0 right-0 bottom-0 py-2 px-4 bg-white">
-				<div className="close-menu flex justify-end">
-					<button onClick={ toggleMenuMobile } type="button" className='bg-tertiary rounded-lg p-2'>
-						<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox='0 0 48 48'><path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" /></svg>
-					</button>
-				</div>
-				<div className="input-search-modal mb-7">
-					<input ref={ mobileInputRef } className="border-black border w-full mt-7 rounded-lg py-2 px-3 placeholder:text-black" type="text" name="search-modal" placeholder="Search for a cat" />
-				</div>
-				<h2 className='text-lg font-bold'>Results</h2>
-				
-			</div>
 		</>
 	)
 }
+
+//#endregion
--- pages/index.js
+++ pages/index.js
@@ -3,35 +3,36 @@ import Head from "next/head"
 import Image from "next/image"
 import Form from "components/Home/Form"
 
-export async function getStaticProps() {
-  const queryClient = new QueryClient()
+// export async function getStaticProps() {
+//   const queryClient = new QueryClient()
 
-  await queryClient.prefetchQuery('posts', () => 
-    fetch('https://api.thecatapi.com/v1/breeds').then(res => res.json())
-  )
+//   await queryClient.prefetchQuery('posts', () => 
+//     fetch('https://api.thecatapi.com/v1/breeds').then(res => res.json())
+//   )
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  }
-}
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   }
+// }
 
 export default function Home() {
-  const { data } = useQuery('posts', () =>
-    fetch('https://api.thecatapi.com/v1/breeds').then(res => res.json())
-  )
+  const data = []
+  // const { data } = useQuery('posts', () =>
+  //   fetch('https://api.thecatapi.com/v1/breeds').then(res => res.json())
+  // )
   console.log(data)
   return (
     <>
       <Head>
         <title>Home de catwiki</title>
       </Head>
-     <section className="hero py-2 px-7 rounded-t-3xl bg-[url('/images/HeroImagesm.png')] bg-no-repeat bg-cover">
+     <section className="hero py-6 px-7 rounded-t-3xl bg-[url('/images/HeroImagesm.png')] bg-no-repeat bg-cover">
       <div className="left w-1/2">
         <Image className="h-[35px] w-16" src="/icons/CatwikiLogo2.svg" width={64} height={35} alt="Otra imagen del gato" />
         <p className="text-white text-xs mb-2">Get to know more about your cat breed</p>
-        <Form data={data} />
+        <Form data={data}/>
       </div>
      </section>
     </>
